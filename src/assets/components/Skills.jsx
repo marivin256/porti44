11e6db0116876999752304ed40d216bfcbbaa8c0
marvin256 <marvintomusange@gmail.com>
2024-07-15 16:12:39 +0300

    other com

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -7,71 +7,85 @@ import Program from "/src/assets/components/Program.jpg"
 
 
 gsap.registerPlugin(ScrollTrigger);
-function Skills () {
+function App () {
+useEffect(() => {
+
+
+  function animateFrom(ele, direction) {
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    if(ele.classList.contains("gs_reveal_fromLeft")) {
+      x = -200;
+      y = 0;
+    } else if (ele.classList.contains("gs_reveal_fromRight")) {
+      x = 100;
+      y = 0;
+    }
+    ele.style.transform = "translate(" + x + "px, " + y + "px)";
+    ele.style.opacity = "0";
+    gsap.fromTo(ele, {x: x, y: y, autoAlpha: 0}, {
+      duration: 1.25, 
+      x: 0,
+      y: 0, 
+      autoAlpha: 1, 
+      ease: "expo", 
+      overwrite: "auto"
+    });
+  }
+  
+  function hide(ele) {
+    gsap.set(ele, {autoAlpha: 0});
+
+  }
 
   
-    useEffect(() => { 
-
-    gsap.fromTo(".box",
-    {
-      
-      autoAlpha: 0, x: -50},{autoAlpha: 1, x: 5, duration:1.75, ease:"expo"},
-      
-
-      
-    )
-
-
-    gsap.fromTo(".lbox",
-    {autoAlpha: 0, x: 50},{autoAlpha: 1, x: -5, duration:1.75}
-    )
-
-    gsap.fromTo(".hed",
-    {autoAlpha: 0, y: 50},{autoAlpha: 1, y: 20, duration:1.50}
-    )
-
-    gsap.fromTo(".gs_reveal",
-    {autoAlpha: 0, y: 50},{autoAlpha: 1, y: 20, duration:1.70}
-    )
-    gsap.fromTo(".top",
-    {autoAlpha: 0, y: 50},{autoAlpha: 1, y: -30, duration:0.55}
-    )
-
-
-  }, []);
   
   
+    gsap.utils.toArray(".gs_reveal").forEach(function(ele) {
+    hide(ele); // assure that the eleent is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: ele,
+      markers: false,
+      onEnter: function() { animateFrom(ele) }, 
+      onEnterBack: function() { animateFrom(ele, -1) },
+      onLeave: function() { hide(ele) } // assure that the eleent is hidden when scrolled into view
+    });
+  });
+    
+})
   
 
   return(
     
 
 <div className="cInnerContent text-white ">
-  <h1 className="header-section top ipsType_center text-yellow-600 text-4xl font-extrabold text-white sm:text-5xl md:text-6xl ">MY SKILLS AND ABILITIES</h1>
+  <h1 className="header-section gs_reveal ipsType_center text-yellow-600 text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">MY SKILLS AND ABILITIES</h1>
   
   <div className="features">
 
     <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
-      <div className=" featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromLeft">
-        <div className="card box">
+      <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromLeft">
+        <div className="card">
           <img width="479" src={Corporation} alt=""/>
         </div>
       </div>
 
       <div className="ipsGrid_span7 ipsType_left">
-        <h2 className="heading_large hed"><strong className="text-yellow-600">CORPERATION SKILLS</strong> </h2>
+        <h2 className="heading_large gs_reveal"><strong className="text-yellow-600">CORPERATION SKILLS</strong> </h2>
         <p className="gs_reveal">As a Software Developer, I often interuct with my colligues and Clients frequently, Which creates and strong bond between me, my colligues and my Clients.</p>
       </div>
     </div>
 
     <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
       <div className="ipsGrid_span7 ipsType_right">
-        <h2 className="heading_large hed"> <strong className="text-yellow-600">DEBUGGING SKILLS</strong> </h2>
+        <h2 className="heading_large gs_reveal"> <strong className="text-yellow-600">DEBUGGING SKILLS</strong> </h2>
         <p className="gs_reveal">Debugging is the basic Developer tool that helps understand code, find problems, and test Application flows.</p>
       </div>
 
       <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromRight">
-        <div className="card lbox">
+        <div className="card">
           <img width="479" src={Debug} alt=""/>
         </div>
       </div>
@@ -79,25 +93,25 @@ function Skills () {
 
     <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
       <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromLeft">
-        <div className="card box">
+        <div className="card">
           <img width="479" src={Problem} alt=""/>
         </div>
       </div>
 
       <div className="ipsGrid_span7 ipsType_left">
-        <h2 className="heading_large hed"> <strong className="text-yellow-600">PROBLEM SOLVING SKILLS</strong> </h2>
+        <h2 className="heading_large gs_reveal"> <strong className="text-yellow-600">PROBLEM SOLVING SKILLS</strong> </h2>
         <p className="gs_reveal">As a Software Developer, I am able to quickly identify problems and offer effective solutions.</p>
       </div>
     </div>
 
     <div className="feature ipsSpacer_bottom_double ipsGrid ipsGrid_collapsePhone">
       <div className="ipsGrid_span7 ipsType_right">
-        <h2 className="heading_large gs_head"><strong className="text-yellow-600">PROGRAMMING SKILLS</strong></h2>
+        <h2 className="heading_large gs_reveal"><strong className="text-yellow-600">PROGRAMMING SKILLS</strong></h2>
         <p className="gs_reveal">I have skills in various programming  scripting and frame works such as React and Vue.</p>
       </div>
 
       <div className="featured-image-container ipsGrid_span5 gs_reveal gs_reveal_fromRight">
-        <div className="card lbox">
+        <div className="card">
           <img width="479"  src={Program} alt=""/>
         </div>
       </div>
@@ -111,4 +125,4 @@ function Skills () {
 
   )
 }
-export default Skills;
+export default App;
