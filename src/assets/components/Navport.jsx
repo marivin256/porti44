dce8c1a76596e134738c18fc33f393f3e672881f
marvin256 <marvintomusange@gmail.com>
2024-07-19 01:25:09 +0300

    17th commit

--- src/assets/components/Navport.jsx
+++ src/assets/components/Navport.jsx
@@ -1,60 +1,90 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from '/src/assets/components/logo.jpg';
-function Port (){
-  const linkClass = ({isActive})=> isActive
-  ?'border-b border-yellow-600 text-yellow-600 rounded-md px-3 py-2'
-  :'text-white hover:border-b border-yellow-600 hover:text-yellow-600 rounded-md px-3 py-2'
-    return(
 
-<div className="bg-black sticky top-0 z-50">
- <nav className=" border-b border-yellow-500 ">
-       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-">
-         <div className="flex h-20 items-center justify-between ">
-           <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-               <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img
+function Port() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar border-b border-yellow-500 shadow-lg">
+        <div className="nav-container">
+          <NavLink  to="/" className="nav-logo">
+          <img
                 className="h-10 w-auto z-10 ml-2"
                 src={logo}
                 alt="Marvitech logo"
               />
-              <h1 className="hidden md:block text-white text-4xl font-bold ml-2"
+            
+            <h1 className=" text-white text-4xl font-bold ml-2"
                 >Marvi<span className="text-yellow-500">Tech</span></h1>
-              
-            </NavLink>
-                <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <NavLink
-                  to="/"
-                  className={linkClass}>
-                  Home</NavLink>
-                
-                <NavLink
-                  to="/about"
-                  className={linkClass}>
-                  About</NavLink>
-                
-                <NavLink
-                  to="/services"
-                  className={linkClass}>
-                  Services</NavLink>
-                
-                <NavLink
-                  to="/skills"
-                  className={linkClass}>
-                  Skills</NavLink>
-                
-                
-               
-              </div>
-            </div>
+          </NavLink>
 
-           </div>
-        </div>        
-       </div>
-   </nav>
-</div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                
+                to="/services"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                
+                to="/skills"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+              Skills
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            
 
-    )
+            {click ? (
+              <span className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"/></svg> {" "}
+              </span>
+            ) : (
+              <span className="icon">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M20.75 7a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75m0 5a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75m0 5a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75" clip-rule="evenodd"/></svg>
+             
+              </span>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
+
 export default Port;
