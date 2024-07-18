ee3bf4b1973c0555d6d0f6aead8363c897db874c
marvin256 <marvintomusange@gmail.com>
2024-07-19 01:39:26 +0300

    17th commit

--- src/assets/components/Navport.jsx
+++ src/assets/components/Navport.jsx
@@ -71,11 +71,11 @@ function Port() {
             
 
             {click ? (
-              <span className="icon">
+              <span className="icon text-4xl">
               <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"/></svg> {" "}
               </span>
             ) : (
-              <span className="icon">
+              <span className="icon text-4xl">
                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M20.75 7a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75m0 5a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75m0 5a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75" clip-rule="evenodd"/></svg>
              
               </span>
